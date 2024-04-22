c95698ea7a8877e80c20041aac9d70cb2e23ff49
Michal <mwargan@gmail.com>
2024-04-22 20:03:33 +0000

    feat: added 429 view

---
+++ src/views/429View.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import BaseButton from "@/components/BaseButton.vue";
+</script>
+<template>
+  <div>
+    <h1>429</h1>
+    <p>
+      {{
+        $t(
+          "We've detected an unusual amount of traffic from your IP address. Please wait a few minutes before trying again."
+        )
+      }}
+    </p>
+    <base-button to="/">{{ $t("Home") }}</base-button>
+  </div>
+</template>
