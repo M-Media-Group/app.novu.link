beefb76492b3b951e909390107754258d8b87a75
Michal <mwargan@gmail.com>
2024-04-24 10:20:22 +0000

    feat: add create redirect form

---
+++ src/forms/CreateRedirect.vue
@@ -0,0 +1,136 @@
+<script setup lang="ts">
+import BaseForm from "@/forms/BaseForm.vue";
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import { eventTypes, useEventsBus } from "@/eventBus/events";
+import { useI18n } from "vue-i18n";
+import { formatUrl } from "@/helpers/urlFormatter";
+import { debounce } from "@/helpers/debounce";
+import { useUserStore } from "@/stores/user";
+
+const $bus = useEventsBus();
+
+const name = ref("");
+const defaultEndpoint = ref("");
+
+const { t } = useI18n();
+
+const isLoading = ref(false);
+
+const baseFormRef = ref();
+
+const router = useRouter();
+
+const userStore = useUserStore();
+
+const randomName = () => {
+  const wordList = [
+    "apple",
+    "banana",
+    "cherry",
+    "date",
+    "elderberry",
+    "fig",
+    "grape",
+    "honeydew",
+    "kiwi",
+    "lemon",
+    "mango",
+    "nectarine",
+    "orange",
+    "pear",
+    "quince",
+    "raspberry",
+    "strawberry",
+    "tangerine",
+    "watermelon",
+  ];
+
+  name.value = wordList[Math.floor(Math.random() * wordList.length)];
+};
+
+randomName();
+
+const debounceAddProtocolIfMissing = debounce(
+  (data: string) =>
+    defaultEndpoint.value
+      ? (defaultEndpoint.value = formatUrl(data))
+      : undefined,
+  500,
+  true
+);
+
+const submitForm = async () => {
+  isLoading.value = true;
+  //   If the URL does not start with http, auto append https://
+  if (!defaultEndpoint.value.startsWith("http")) {
+    defaultEndpoint.value = `https://${defaultEndpoint.value}`;
+  }
+
+  const response = await axios
+    .post("/redirects", {
+      name: name.value,
+      default_endpoint: defaultEndpoint.value,
+    })
+    .catch((error) => {
+      isLoading.value = false;
+      // If the error is not a validation error, show a generic error message
+      if (!error.response || error.response.status !== 422)
+        alert(t("An error occurred. Please try again later."));
+
+      return error.response;
+    });
+
+  if (response.status === 201) {
+    baseFormRef.value.setSuccessOnInputs();
+    $bus.$emit(eventTypes.created_redirect);
+    // If not logged in, redirect to "/login/otp"
+    if (!userStore.isAuthenticated) {
+      router.push({
+        name: "login-otp",
+        query: { redirect: `/redirects/${response.data.uuid}` },
+      });
+      return;
+    }
+    router.push(`/redirects/${response.data.uuid}`);
+  } else {
+    console.log(response);
+    baseFormRef.value.setInputErrors(response.data.errors);
+  }
+
+  isLoading.value = false;
+};
+</script>
+<template>
+  <base-form
+    :autofocus="false"
+    :submitText="$t('Create a free permanent magic link')"
+    @submit="submitForm"
+    ref="baseFormRef"
+    :isLoading="isLoading"
+  >
+    <label for="name">{{ $t("Magic link name") }}</label
+    ><input id="name" type="text" required v-model="name" />
+    <label for="default_endpoint"> {{ $t("Go to") }} </label
+    ><input
+      name="default_endpoint"
+      type="url"
+      inputmode="url"
+      minlength="10"
+      autocapitalize="none"
+      placeholder="https://test.com"
+      data-hj-allow=""
+      pattern="(https?://)?([a-z0-9\-]+\.)+[a-z]{2,}(:[0-9]+)?(/.*)?"
+      required
+      v-model="defaultEndpoint"
+      autofocus
+      @input="
+        debounceAddProtocolIfMissing(($event.target as HTMLInputElement).value)
+      "
+    />
+    <small>
+      {{ $t("This is where your magic link will redirect to by default.") }}
+    </small>
+  </base-form>
+</template>
