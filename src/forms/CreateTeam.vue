7c0eff07e6ccc83e012755f22f03a932602b0154
Michal <mwargan@gmail.com>
2024-04-24 00:12:09 +0000

    Add CreateTeam.vue component for team creation

---
+++ src/forms/CreateTeam.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { useTeamStore } from "@/stores/team";
+import { ref } from "vue";
+import BaseForm from "./BaseForm.vue";
+
+const teamStore = useTeamStore();
+
+const name = ref("");
+
+const baseFormRef = ref();
+
+const emit = defineEmits(["success"]);
+
+// The submit function. If there is just the email, check if the email is valid. If it is not, set the register mode. If it is, set the login mode.
+const submitForm = async () => {
+  if (!name.value) {
+    return;
+  }
+
+  const response = await teamStore.createTeam({
+    name: name.value,
+  });
+
+  if (response === true) {
+    // Emit the updated event with the changed fields
+    emit("success");
+    baseFormRef.value.setSuccessOnInputs();
+  } else if (typeof response === "object") {
+    // Show the fields with errors
+    baseFormRef.value.setInputErrors(response.data.errors);
+  }
+};
+</script>
+
+<template>
+  <base-form ref="baseFormRef" @submit="submitForm">
+    <!-- The form starts with just the email. The user presses a button and we check if we should show the register or login inputs -->
+    <!-- <TransitionGroup> -->
+
+    <!-- Name, Surname, and new password inputs NOTE THE PATTERN - needed to trigger validity on non-dirty (script added) inputs, see https://stackoverflow.com/a/53261163/7410951 -->
+    <label for="name">{{ $t("Team Name") }}</label>
+    <input
+      type="text"
+      id="name"
+      name="name"
+      :placeholder="$t('Team Name')"
+      v-model="name"
+      minlength="2"
+      pattern=".{2,}"
+      autofocus
+      required
+    />
+    <!-- </TransitionGroup> -->
+  </base-form>
+</template>
