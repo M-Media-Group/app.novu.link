1f91bb3bee38a874847c312ea42930cba23ad0ab
Michal <mwargan@gmail.com>
2024-04-24 10:32:25 +0000

    feat: added redirect-view

---
+++ src/views/RedirectsView.vue
@@ -0,0 +1,61 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import type { Redirect } from "@/types/redirect";
+import CardElement from "@/components/CardElement.vue";
+import { getRedirectQrCodeUrl, getRedirects } from "@/useRedirects";
+import { removeProtocol } from "@/helpers/urlFormatter";
+
+const redirects = ref([] as Redirect[]);
+const isLoading = ref(true);
+
+getRedirects().then((response) => {
+  redirects.value = response.data;
+  // Sort by total clicks
+  redirects.value.sort((a, b) => b.todays_clicks_count - a.todays_clicks_count);
+  isLoading.value = false;
+});
+
+const defaultEndpoint = (redirect: Redirect) => {
+  const defaultEndpoint = redirect.endpoints.find(
+    (endpoint) => endpoint.is_default
+  )?.endpoint;
+  if (!defaultEndpoint) {
+    return null;
+  }
+  return removeProtocol(defaultEndpoint);
+};
+</script>
+<template>
+  <hgroup>
+    <h1>{{ $t("Magic links") }}</h1>
+    <p>All links</p>
+  </hgroup>
+  <div>
+    <card-element
+      v-for="redirect in redirects"
+      :key="redirect.uuid"
+      @click="$router.push(`/redirects/${redirect.uuid}`)"
+      :title="redirect.name"
+      :subtitle="
+        redirect.todays_clicks_count +
+        ' ' +
+        $t('Scans today').toLocaleLowerCase() +
+        ' - ' +
+        // Defaults to
+        defaultEndpoint(redirect) +
+        (redirect.endpoints.length > 1
+          ? ' + ' + (redirect.endpoints.length - 1) + ' ' + $t('more')
+          : '')
+      "
+    >
+      <template #headerActions>
+        <img
+          height="64"
+          width="64"
+          :src="getRedirectQrCodeUrl(redirect.uuid)"
+          alt="QR code"
+        />
+      </template>
+    </card-element>
+  </div>
+</template>
