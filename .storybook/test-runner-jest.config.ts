eab5f5072ba6bc85221aac8873fd9a0ae7dae3ae
Michal <mwargan@gmail.com>
2024-04-24 14:47:12 +0000

    tests: increase storybook timeout

---
+++ .storybook/test-runner-jest.config.ts
@@ -0,0 +1,15 @@
+const { getJestConfig } = require('@storybook/test-runner');
+
+const testRunnerConfig = getJestConfig();
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
+module.exports = {
+    // The default Jest configuration comes from @storybook/test-runner
+    ...testRunnerConfig,
+    /** Add your own overrides below
+     * @see https://jestjs.io/docs/configuration
+     */
+    testTimeout: 20000, // default timeout is 15s
+};
