bbc2878fac9fe58bec6caf809d619dfac0f776d1
Michal <mwargan@gmail.com>
2024-04-25 07:46:18 +0000

    feat: added initial homepage text

---
+++ src/stories/HomePage.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import HomeView from "@/views/HomeView.vue";
+// Import the user fixture json data
+
+const meta: Meta<typeof HomeView> = {
+  title: "Page/HomeView",
+  component: HomeView,
+
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: "fullscreen",
+  },
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof HomeView>;
+
+// More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
+export const NoUserData: Story = {};
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,37 @@
 <script setup lang="ts">
 import CreateRedirect from "@/forms/CreateRedirect.vue";
 import CardElement from "@/components/CardElement.vue";
+import { useI18n } from "vue-i18n";
+import { ref, watch } from "vue";
+
+const { t, locale } = useI18n();
+
+const featureData = ref([] as any[]);
+const testimonialData = ref([] as any[]);
+const faqData = ref([] as any[]);
+const goodPointsData = ref([] as any[]);
+const painPointsData = ref([] as any[]);
+
+// Load the features from the correct locale
+const loadData = (dataset = "features", localeToUse = locale.value) => {
+  return import(`@/data/${dataset}/${localeToUse}.json`).then(
+    (module) => module.default
+  );
+};
+
+watch(
+  locale,
+  async (newLocale) => {
+    featureData.value = await loadData("features", newLocale);
+    testimonialData.value = await loadData("testimonials", newLocale);
+    faqData.value = await loadData("faqs", newLocale);
+    goodPointsData.value = await loadData("goodPoints", newLocale);
+    painPointsData.value = await loadData("painPoints", newLocale);
+  },
+  {
+    immediate: true,
+  }
+);
 </script>
 
 <template>
@@ -18,6 +49,65 @@ import CardElement from "@/components/CardElement.vue";
     <card-element>
       <create-redirect :autofocus="false"></create-redirect>
     </card-element>
+  </section>
+
+  <section id="testimonials">
+    <h2>{{ $t("What our customers say") }}</h2>
+    <ul>
+      <li v-for="testimonial in testimonialData" :key="testimonial.id">
+        <hgroup>
+          <h3>{{ testimonial.name }}</h3>
+          <p v-if="testimonial.jobTitle">{{ testimonial.jobTitle }}</p>
+        </hgroup>
+        <p>{{ testimonial.description }}</p>
+      </li>
+    </ul>
+  </section>
+
+  <section id="painPoints">
+    <h2>{{ $t("Situations Novu.Link saves you from") }}</h2>
+    <ul>
+      <li v-for="painPoint in painPointsData" :key="painPoint.id">
+        <hgroup>
+          <h3>{{ painPoint.name }}</h3>
+        </hgroup>
+        <p>{{ painPoint.description }}</p>
+      </li>
+    </ul>
+  </section>
+
+  <section id="goodPoints">
+    <h2>{{ $t("How Novu.Link does even more") }}</h2>
+    <ul>
+      <li v-for="goodPoint in goodPointsData" :key="goodPoint.id">
+        <hgroup>
+          <h3>{{ goodPoint.name }}</h3>
+        </hgroup>
+        <p>{{ goodPoint.description }}</p>
+      </li>
+    </ul>
+  </section>
+
+  <section id="features">
+    <h2>{{ $t("Features") }}</h2>
+    <ul>
+      <li v-for="feature in featureData" :key="feature.id">
+        <hgroup>
+          <h3>{{ feature.name }}</h3>
+          <p>{{ feature.min_subscription === 0 ? "Free" : "Pro" }}</p>
+        </hgroup>
+        <p>{{ feature.description }}</p>
+      </li>
+    </ul>
+  </section>
+
+  <section id="faq">
+    <h2>{{ $t("FAQ") }}</h2>
+    <!-- For FAQ we will use a details -->
+    <details v-for="faq in faqData" :key="faq.id">
+      <summary>{{ faq.question }}</summary>
+      <p>{{ faq.answer }}</p>
+    </details>
   </section>
 </template>
 <style scoped>
