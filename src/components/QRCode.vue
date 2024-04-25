289d880d34dc810729bfd4eaf0859fde2d49b220
Michal <mwargan@gmail.com>
2024-04-25 17:31:49 +0000

    refactor: QR code generator

---
+++ src/components/QRCode.vue
@@ -0,0 +1,137 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+import LinkReady from "@/assets/linkReady.png";
+
+import QRCodeStyling from "qr-code-styling";
+import type { PropType } from "vue";
+import { getRedirectQrCodeDataUrl } from "@/useRedirects";
+
+const props = defineProps({
+  /** The UUID of the redirect */
+  redirectId: {
+    type: String,
+    required: true,
+  },
+  lightColor: {
+    type: String,
+    default: "#ffffff",
+  },
+  darkColor: {
+    type: String,
+    default: "#000000",
+  },
+  logoDataUrl: {
+    type: String as PropType<string | null>,
+    default: null,
+  },
+  selectedShape: {
+    type: String as PropType<"square" | "rounded" | "circle">,
+    default: "square",
+  },
+  dimensions: {
+    type: Number,
+    default: 240,
+  },
+});
+
+const emit = defineEmits<{
+  update: [string | null];
+}>();
+
+const qrCodeDataURL = ref<string | null>(null);
+
+const compute2 = async (
+  urlToEncode = "",
+  lighColor = "#ffffff",
+  darkColor = "#000000",
+  logoDataUrl: string | null = null,
+  shape = "square" as "square" | "rounded" | "circle"
+) => {
+  const qrCode = new QRCodeStyling({
+    width: 480,
+    height: 480,
+    type: "svg",
+    data: urlToEncode,
+    image: logoDataUrl ?? undefined,
+    margin: 2,
+    dotsOptions: {
+      color: darkColor,
+      type: shape === "circle" ? "rounded" : shape,
+    },
+    backgroundOptions: {
+      color: lighColor,
+    },
+    cornersSquareOptions: {
+      color: darkColor,
+      // type: shape === "rounded" ? "square" : shape,
+    },
+    cornersDotOptions: {
+      color: darkColor,
+      // type: shape === "rounded" ? "dot" : shape,
+    },
+    imageOptions: {
+      crossOrigin: "anonymous",
+      margin: 2,
+    },
+    qrOptions: {
+      errorCorrectionLevel: "L",
+    },
+  });
+
+  const data = (await qrCode.getRawData()) as Blob;
+  // The above is a blob, we need to convert it to a data URL
+  const reader = new FileReader();
+  reader.readAsDataURL(data);
+  reader.onload = () => {
+    qrCodeDataURL.value = reader.result as string;
+    emit("update", qrCodeDataURL.value);
+  };
+  return reader.result as string;
+};
+
+const imgSrc = ref<string | null>(null);
+
+// Watch for all prop changes
+watch(
+  () => [
+    props.lightColor,
+    props.darkColor,
+    props.logoDataUrl,
+    props.selectedShape,
+    props.redirectId,
+  ],
+  () => {
+    compute2(
+      getRedirectQrCodeDataUrl(props.redirectId),
+      props.lightColor,
+      props.darkColor,
+      props.logoDataUrl,
+      props.selectedShape
+    );
+  },
+  {
+    immediate: true,
+  }
+);
+</script>
+
+<template>
+  <div class="main-grid-display smaller-gap">
+    <img
+      :height="dimensions"
+      :width="dimensions"
+      :src="qrCodeDataURL ?? imgSrc ?? LinkReady"
+      alt="QR code"
+    />
+  </div>
+</template>
+<style scoped>
+/* The image should be 100% to a maximum of 400px */
+img {
+  aspect-ratio: 1;
+
+  margin: 0 auto;
+
+  border-radius: var(--pico-border-radius);
+}
+</style>
---
+++ src/stories/Components/QRCode.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+import QRCode from "@/components/QRCode.vue";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta: Meta<typeof QRCode> = {
+  title: "Components/QRCode",
+  component: QRCode,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  parameters: {
+    // Disable aria-hidden-focus for this story
+    a11y: {
+      config: {
+        /** @todo actually find a nice way to fix the color contrast issue on this graph */
+        rules: [{ id: "color-contrast", enabled: false }],
+      },
+    },
+  },
+  args: {
+    redirectId: "1234",
+  },
+
+  // The argType for selectedShape should be square, rounded, or circle
+  argTypes: {
+    selectedShape: {
+      options: ["square", "rounded", "circle"],
+      control: { type: "select" },
+      table: { category: "Props" },
+      description: "The shape of the QR code",
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof QRCode>;
+
+export const Default: Story = {};
