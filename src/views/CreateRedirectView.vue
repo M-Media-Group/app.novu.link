bcd4893910c2b5b965a9d0e918363ed6081f0a7b
Michal <mwargan@gmail.com>
2024-04-27 19:11:49 +0000

    feat: add image to create redirect view

--- src/views/CreateRedirectView.vue
+++ src/views/CreateRedirectView.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import CreateRedirect from "@/forms/CreateRedirect.vue";
 import CardElement from "@/components/CardElement.vue";
+import image from "@/assets/undraw_connection.svg";
 </script>
 <template>
   <h1>{{ $t("New magic link") }}</h1>
-  <card-element>
-    <create-redirect></create-redirect>
-  </card-element>
+  <div class="two-column-grid">
+    <card-element>
+      <create-redirect></create-redirect>
+    </card-element>
+    <img :src="image" alt="A person holding a link" />
+  </div>
 </template>
