64156e27dedbff4210b85c038237a67f36e269e7
Michal <mwargan@gmail.com>
2024-04-27 13:09:34 +0000

    tests: added gatekeeper to storybook, set userStore ready by default

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -8,14 +8,18 @@ import { setup } from "@storybook/vue3";
 import { type App } from "vue";
 import { EventsPlugin } from "../src/eventBus/events";
 import { createPinia } from "pinia";
-import { navIsLoading } from "../src/router";
+import router, { navIsLoading } from "../src/router";
 import { vueRouter } from "storybook-vue3-router";
 import axios from "axios";
+import { gatePlugin } from "@m-media/vue3-gate-keeper/src/gateKeeper";
+import gates from "../src/router/gates";
+import { useUserStore } from "../src/stores/user";
 
 setup((app: App) => {
   app.use(createPinia());
   app.use(i18n);
   app.use(EventsPlugin);
+  app.use(gatePlugin, { gateInstances: gates }, router);
 
   /** We need to set navIsLoading to false otherwise it never resolves becasuse the router guards don't seem to be called @todo check this */
   navIsLoading.value = false;
@@ -24,6 +28,9 @@ setup((app: App) => {
   axios.defaults.withCredentials = true;
   axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
+  const store = useUserStore();
+  store.attemptedToFetchUser = true;
+  store.isLoading = false;
 });
 
 const preview: Preview = {
