032f668d560418f00976baa800c1defb89c7ae8b
Michal <mwargan@gmail.com>
2024-04-30 15:19:32 +0000

    feat: added image to create team screen

--- src/views/CreateTeamView.vue
+++ src/views/CreateTeamView.vue
@@ -2,6 +2,7 @@
 import CardElement from "@/components/CardElement.vue";
 import CreateTeam from "@/forms/CreateTeam.vue";
 import { useRouter } from "vue-router";
+import image from "@/assets/undraw_selecting_team_re_ndkb.svg";
 
 const router = useRouter();
 
@@ -11,7 +12,10 @@ const handleSuccess = () => {
 </script>
 <template>
   <h1>{{ $t("New Team") }}</h1>
-  <card-element :titleHeadingLevel="2" :showTitle="false">
-    <create-team @success="handleSuccess"></create-team>
-  </card-element>
+  <div class="two-column-grid">
+    <card-element :titleHeadingLevel="2" :showTitle="false">
+      <create-team @success="handleSuccess"></create-team>
+    </card-element>
+    <img :src="image" alt="A person holding a link" />
+  </div>
 </template>
