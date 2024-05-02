39bcc72048f3acdb990ec5ae5811b771227b277e
Michal <mwargan@gmail.com>
2024-05-02 12:15:51 +0000

    feat: improved 404

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
 import BaseButton from "@/components/BaseButton.vue";
+import CardElement from "@/components/CardElement.vue";
+import image from "@/assets/undraw_team_spirit_re_yl1v.svg";
 </script>
 <template>
   <div>
-    <h1>404</h1>
-    <base-button to="/">{{ $t("Home") }}</base-button>
+    <hgroup>
+      <h1>404</h1>
+      <p>This page can't be found.</p>
+    </hgroup>
+    <div class="two-column-grid">
+      <card-element title="Lets start again">
+        <base-button to="/">{{ $t("Go to the homepage") }}</base-button>
+      </card-element>
+      <img :src="image" alt="A person holding a link" />
+    </div>
   </div>
 </template>
