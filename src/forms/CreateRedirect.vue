8028a85338c90aec69fb68575f462e7a4f708e06
Michal <mwargan@gmail.com>
2024-08-27 14:42:44 +0000

    removed placeholder to make it clearer field is empty

--- src/forms/CreateRedirect.vue
+++ src/forms/CreateRedirect.vue
@@ -145,7 +145,7 @@ const submitForm = async () => {
       inputmode="url"
       minlength="10"
       autocapitalize="none"
-      placeholder="example.com"
+      placeholder=""
       data-hj-allow=""
       pattern="(https?://)?([a-z0-9\-]+\.)+[a-z]{2,}(:[0-9]+)?(/.*)?"
       required
