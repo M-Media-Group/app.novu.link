6250605cd1887884b7770f391f7353c04ced2a95
Michal <mwargan@gmail.com>
2025-05-17 18:49:20 +0000

    refactor: implement useForm composable for form validation and error handling

---
+++ src/composables/useForm.ts
@@ -0,0 +1,207 @@
+import { type Ref, ref } from "vue";
+
+const supportedElements = [
+  HTMLInputElement,
+  HTMLSelectElement,
+  HTMLTextAreaElement,
+  HTMLFormElement,
+  HTMLFieldSetElement,
+] as const;
+
+type HTMLSupportedInputElement =
+  (typeof supportedElements)[number] extends new (...args: any[]) => infer R
+    ? R
+    : never;
+
+const setErrorMessageOnElement = (element: HTMLSupportedInputElement) => {
+  //  Update or create a sibling element with the error message
+  let errorElement = element.nextElementSibling as HTMLElement;
+  if (!errorElement || !errorElement.classList.contains("error")) {
+    errorElement = document.createElement("small");
+    element.after(errorElement);
+  }
+  errorElement.innerText = element.validationMessage;
+  errorElement.classList.add("error");
+
+  element.insertAdjacentElement("afterend", errorElement);
+};
+
+const setErrorOnInput = (
+  input: HTMLSupportedInputElement,
+  error: string,
+  report = true
+) => {
+  //   Use setValidity to set the error message
+  input.setCustomValidity(error);
+  input.setAttribute("aria-invalid", "true");
+  if (report) {
+    input.reportValidity();
+  }
+  setErrorMessageOnElement(input);
+};
+
+const setSuccessOnInput = (input: HTMLSupportedInputElement) => {
+  input.setAttribute("aria-invalid", "false");
+};
+
+const removeSuccessOnInput = (input: HTMLSupportedInputElement) => {
+  if (input.validity.valid) {
+    input.removeAttribute("aria-invalid");
+  }
+};
+
+const resetCustomValidityOnInput = (input: HTMLSupportedInputElement) => {
+  input.setCustomValidity("");
+  clearErrorMessageOnElement(input);
+};
+
+const clearErrorMessageOnElement = (element: HTMLSupportedInputElement) => {
+  // If the element is valid, remove the invalid class
+  const errorElement = element.nextElementSibling as HTMLElement;
+  if (errorElement && errorElement.classList.contains("error")) {
+    errorElement.remove();
+  }
+};
+
+const isElementInFocus = (element: HTMLSupportedInputElement) => {
+  return document.activeElement === element;
+};
+
+export const useForm = (formElement: Ref<HTMLFormElement | null>) => {
+  const formIsValid = ref(false);
+
+  const focusOnFirstInput = () => {
+    const firstInput = formElement.value?.querySelector("input");
+    if (firstInput) {
+      firstInput.focus();
+    }
+  };
+
+  const focusOnFirstEmptyInput = () => {
+    const firstInput = formElement.value?.querySelectorAll("input");
+    if (firstInput) {
+      for (let i = 0; i < firstInput.length; i++) {
+        if (!firstInput[i].value) {
+          firstInput[i].focus();
+          return;
+        }
+      }
+    }
+  };
+
+  const checkValidity = () => {
+    checkValidityOnAllInputs();
+
+    formIsValid.value = formElement.value?.checkValidity() ?? false;
+  };
+
+  const handleInput = () => {
+    resetCustomValidityOnInputs();
+    checkValidity();
+  };
+
+  const callActionsOnAllInputs = (
+    callback: (element: HTMLSupportedInputElement) => void
+  ) => {
+    if (!formElement.value?.elements) {
+      return;
+    }
+    const elements = formElement.value.elements;
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      if (
+        element instanceof HTMLInputElement ||
+        element instanceof HTMLSelectElement ||
+        element instanceof HTMLTextAreaElement ||
+        element instanceof HTMLFormElement ||
+        element instanceof HTMLFieldSetElement
+      ) {
+        callback(element);
+      }
+    }
+  };
+
+  const resetCustomValidityOnInputs = () => {
+    // For each element in the form, check if it's valid
+    callActionsOnAllInputs((element) => {
+      resetCustomValidityOnInput(element);
+    });
+  };
+
+  const checkValidityOnAllInputs = () => {
+    // For each element in the form, check if it's valid
+    callActionsOnAllInputs((element) => {
+      // If the element is invalid, add the invalid class
+      if (
+        !element.validity.valid &&
+        !element.validity.valueMissing
+
+        // Not sure why below line was here in the first place @todo investigate
+        // !isElementInFocus(element)
+      ) {
+        setErrorMessageOnElement(element);
+      } else {
+        clearErrorMessageOnElement(element);
+      }
+    });
+  };
+
+  const removeSuccessOnInputs = () => {
+    callActionsOnAllInputs((element) => {
+      removeSuccessOnInput(element);
+    });
+  };
+
+  const setInputErrors = (
+    errors?: Record<string, string | string[] | null>
+  ) => {
+    // For each key in errors, find the input and call setErrorOnInput with the value
+    if (!errors) {
+      return;
+    }
+
+    for (const [key, value] of Object.entries(errors)) {
+      const input = formElement.value?.elements.namedItem(
+        key
+      ) as HTMLSupportedInputElement;
+      if (input) {
+        // If the value is an array, join it with a space
+        let valueToPass = value as string | string[];
+
+        // If the valueToPass is an array, join it with a space
+        if (Array.isArray(valueToPass)) {
+          valueToPass = valueToPass.join(" ");
+        }
+
+        setErrorOnInput(input, valueToPass);
+      }
+    }
+  };
+
+  const setSuccessOnInputs = () => {
+    callActionsOnAllInputs((element) => {
+      setSuccessOnInput(element);
+    });
+    // After 5 seconds, clear the success state
+    setTimeout(() => {
+      removeSuccessOnInputs();
+    }, 5000);
+  };
+
+  return {
+    formElement,
+    formIsValid,
+    setErrorOnInput,
+    setInputErrors,
+    setSuccessOnInputs,
+    setSuccessOnInput,
+    removeSuccessOnInputs,
+    removeSuccessOnInput,
+    resetCustomValidityOnInput,
+    resetCustomValidityOnInputs,
+    checkValidity,
+    handleInput,
+    focusOnFirstInput,
+    focusOnFirstEmptyInput,
+  };
+};
--- src/forms/BaseForm.vue
+++ src/forms/BaseForm.vue
@@ -7,15 +7,10 @@ import {
   onMounted,
   onUpdated,
   ref,
+  useTemplateRef,
 } from "vue";
 import { assertIsUnifiedError } from "@/services/apiServiceErrorHandler";
-
-type HTMLSupportedInputElement =
-  | HTMLInputElement
-  | HTMLSelectElement
-  | HTMLTextAreaElement
-  | HTMLFormElement
-  | HTMLFieldSetElement;
+import { useForm } from "@/composables/useForm";
 
 // Prop of submit text
 const props = defineProps({
@@ -29,7 +24,7 @@ const props = defineProps({
     type: String,
     required: false,
   },
-  /** The types of fields that should be cleared if the request fails. By default, these are all inputs of type password. */
+  /** The types of fields that should be cleared if the request fails. By default, these are all inputs of type password. @TODO not yet implemented */
   clearInputTypesOnFailure: {
     type: Array as PropType<string[]>,
     default: () => ["password"],
@@ -78,50 +73,9 @@ const emit = defineEmits<{
   (e: "submit"): void;
 }>();
 
-const formElement = ref<HTMLFormElement>();
-
-const formIsValid = ref(false);
+const formElement = useTemplateRef("formElement");
 
 const loading = ref(false);
-
-const checkValidity = () => {
-  // For each element in the form, check if it's valid
-  callActionsOnAllInputs((element) => {
-    // If the element is invalid, add the invalid class
-    if (
-      !element.validity.valid &&
-      !element.validity.valueMissing &&
-      !isElementInFocus(element)
-    ) {
-      setErrorMessageOnElement(element);
-    } else {
-      clearErrorMessageOnElement(element);
-    }
-  });
-
-  formIsValid.value = formElement.value?.checkValidity() ?? false;
-};
-
-const setErrorMessageOnElement = (element: HTMLSupportedInputElement) => {
-  //  Update or create a sibling element with the error message
-  let errorElement = element.nextElementSibling as HTMLElement;
-  if (!errorElement || !errorElement.classList.contains("error")) {
-    errorElement = document.createElement("small");
-    element.after(errorElement);
-  }
-  errorElement.innerText = element.validationMessage;
-  errorElement.classList.add("error");
-
-  element.insertAdjacentElement("afterend", errorElement);
-};
-
-const clearErrorMessageOnElement = (element: HTMLSupportedInputElement) => {
-  // If the element is valid, remove the invalid class
-  const errorElement = element.nextElementSibling as HTMLElement;
-  if (errorElement && errorElement.classList.contains("error")) {
-    errorElement.remove();
-  }
-};
 
 const submit = async () => {
   if (
@@ -137,7 +91,13 @@ const submit = async () => {
         emit("success", response);
         setSuccessOnInputs();
       } catch (error) {
-        assertIsUnifiedError(error);
+        try {
+          assertIsUnifiedError(error);
+        } catch (e) {
+          console.error("Error is not a UnifiedError", e);
+          alert("An error occurred");
+          return;
+        }
 
         if (error.type === "validation") {
           setSuccessOnInputs();
@@ -154,106 +114,15 @@ const submit = async () => {
   }
 };
 
-const callActionsOnAllInputs = (
-  callback: (element: HTMLSupportedInputElement) => void
-) => {
-  if (!formElement.value?.elements) {
-    return;
-  }
-  const elements = formElement.value.elements;
-  for (let i = 0; i < elements.length; i++) {
-    const element = elements[i];
-    if (
-      element instanceof HTMLInputElement ||
-      element instanceof HTMLSelectElement ||
-      element instanceof HTMLTextAreaElement ||
-      element instanceof HTMLFormElement ||
-      element instanceof HTMLFieldSetElement
-    ) {
-      callback(element);
-    }
-  }
-};
-
-const setInputErrors = (errors?: Record<string, string | string[] | null>) => {
-  // For each key in errors, find the input and call setErrorOnInput with the value
-  if (!errors) {
-    return;
-  }
-
-  for (const [key, value] of Object.entries(errors)) {
-    const input = formElement.value?.elements.namedItem(
-      key
-    ) as HTMLSupportedInputElement;
-    if (input) {
-      // If the value is an array, join it with a space
-      let valueToPass = value as string | string[];
-
-      // If the valueToPass is an array, join it with a space
-      if (Array.isArray(valueToPass)) {
-        valueToPass = valueToPass.join(" ");
-      }
-
-      setErrorOnInput(input, valueToPass);
-    }
-  }
-};
-
-const setErrorOnInput = (
-  input: HTMLSupportedInputElement,
-  error: string,
-  report = true
-) => {
-  //   Use setValidity to set the error message
-  input.setCustomValidity(error);
-  input.setAttribute("aria-invalid", "true");
-  if (report) {
-    input.reportValidity();
-  }
-  setErrorMessageOnElement(input);
-};
-
-const setSuccessOnInputs = () => {
-  callActionsOnAllInputs((element) => {
-    setSuccessOnInput(element);
-  });
-  // After 5 seconds, clear the success state
-  setTimeout(() => {
-    removeSuccessOnInputs();
-  }, 5000);
-};
-
-const setSuccessOnInput = (input: HTMLSupportedInputElement) => {
-  input.setAttribute("aria-invalid", "false");
-};
-
-const removeSuccessOnInputs = () => {
-  callActionsOnAllInputs((element) => {
-    removeSuccessOnInput(element);
-  });
-};
-
-const removeSuccessOnInput = (input: HTMLSupportedInputElement) => {
-  if (input.validity.valid) {
-    input.removeAttribute("aria-invalid");
-  }
-};
-
-const resetCustomValidityOnInput = (input: HTMLSupportedInputElement) => {
-  input.setCustomValidity("");
-  clearErrorMessageOnElement(input);
-};
-
-const resetCustomValidityOnInputs = () => {
-  // For each element in the form, check if it's valid
-  callActionsOnAllInputs((element) => {
-    resetCustomValidityOnInput(element);
-  });
-};
-
-const isElementInFocus = (element: HTMLSupportedInputElement) => {
-  return document.activeElement === element;
-};
+const {
+  formIsValid,
+  checkValidity,
+  setInputErrors,
+  focusOnFirstInput,
+  focusOnFirstEmptyInput,
+  setSuccessOnInputs,
+  handleInput,
+} = useForm(formElement);
 
 // The updated event happens anytime there is input or the slots change
 onUpdated(async () => {
@@ -268,30 +137,6 @@ onMounted(() => {
     focusOnFirstEmptyInput();
   }
 });
-
-const focusOnFirstInput = () => {
-  const firstInput = formElement.value?.querySelector("input");
-  if (firstInput) {
-    firstInput.focus();
-  }
-};
-
-const focusOnFirstEmptyInput = () => {
-  const firstInput = formElement.value?.querySelectorAll("input");
-  if (firstInput) {
-    for (let i = 0; i < firstInput.length; i++) {
-      if (!firstInput[i].value) {
-        firstInput[i].focus();
-        return;
-      }
-    }
-  }
-};
-
-const handleInput = () => {
-  resetCustomValidityOnInputs();
-  checkValidity();
-};
 
 const isAnythingLoading = computed(() => {
   return props.isLoading || loading.value;
